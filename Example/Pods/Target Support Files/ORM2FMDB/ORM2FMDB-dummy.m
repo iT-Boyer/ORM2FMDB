6976f6faf38977beb30fe4cee8539a0b7391ff91
iTBoyer <iTBoyer@qq.com>
2020-04-07 15:33:40 +0800

    init

---
+++ Example/Pods/Target Support Files/ORM2FMDB/ORM2FMDB-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ORM2FMDB : NSObject
+@end
+@implementation PodsDummy_ORM2FMDB
+@end
---
+++ Example/Pods/Target Support Files/Pods-ORM2FMDB_Example/Pods-ORM2FMDB_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ORM2FMDB_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ORM2FMDB_Example
+@end
